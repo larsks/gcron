97bbaab067a15fba9bb9fb373f808614f7ecbe1e
Lars Kellogg-Stedman <lars@arcadia.house>
2009-10-07 19:07:10 -0400

    begin to package-ify

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='netreg',
+        version='0.1',
+        description='automate your computer using Google Calendar',
+        author='Lars Kellogg-Stedman',
+        author_email='lars@oddbit.com',
+        packages=['gcron'],
+        install_requires=['python-dateutil', 'pytz'],
+        )
